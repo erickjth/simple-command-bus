26466eb0b8038ca3c26c37650e334cc213a45c88
Erick Torres <ericktorresh@gmail.com>
2018-04-28 16:43:08 -0500

    Add CreateCommandBusProxy

---
+++ src/CreateCommandBusProxy.js
@@ -0,0 +1,36 @@
+import { capitalize, isDirectory, isFunction, walkSync } from './utils';
+
+const CreateCommandBusProxy = function CreateCommandBusProxy(commandBus, commandsDir) {
+	if (!commandsDir || !isDirectory(commandsDir)) {
+		throw new Error('Invalid commands path.');
+	}
+
+	const availableCommands = walkSync(commandsDir);
+	const cachedCommands = {};
+
+	return new Proxy({}, {
+		get(target, propKey) {
+			const commandName = `${capitalize(propKey)}Command.js`;
+
+			if (!cachedCommands[commandName]) {
+				const foundCommand = availableCommands.find(command => command.endsWith(commandName));
+
+				if (!foundCommand) {
+					throw new Error(`Command "${commandName}" not found.`);
+				}
+
+				cachedCommands[commandName] = require(foundCommand);
+			}
+
+			const Command = cachedCommands[commandName];
+
+			if (isFunction(Command) === false) {
+				throw new Error(`Command "${commandName}" is not callable.`);
+			}
+
+			return (...args) => commandBus.handle(new Command(...args));
+		}
+	});
+};
+
+export default CreateCommandBusProxy;
