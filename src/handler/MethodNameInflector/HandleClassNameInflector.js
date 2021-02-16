cf71af222db2b50005b93b2b5bd54bf1c26394dc
irfanshehzadsandhu <irfanshehzadsandhu@gmail.com>
2021-02-17 04:09:12 +0500

    improvement: remove methodName property from HandleClassNameInflector and use handle hardcoded

--- src/handler/MethodNameInflector/HandleClassNameInflector.js
+++ src/handler/MethodNameInflector/HandleClassNameInflector.js
@@ -1,11 +1,9 @@
 import MethodNameInflector from './MethodNameInflector';
 
 export default class HandleClassNameInflector extends MethodNameInflector {
-    constructor(methodName) {
-        super();
-        this.methodName = methodName || "handle";
-    }
+
     inflect(commandName, handler) {
-        return this.methodName + commandName;
+        return 'handle' + commandName;
     }
+    
 };
